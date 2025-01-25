46ee9673f36923fe4c7cfc12519d743617571c49
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-26 02:24:10 +0530

    file upload in cloudinary

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -4,9 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { v2 as cloudinary } from 'cloudinary';
 
 import { auth } from '@clerk/nextjs/server'
-import { error } from 'console';
-import { resolve } from 'path';
-import { rejects } from 'assert';
+
 
 
 // Configuration
@@ -19,8 +17,8 @@ cloudinary.config({
 //FOR THE TYPESCRIPT
 
 interface CloudinaryUploadResult {
-    pubic_id: string,
-    [key: string]: any
+    public_id: string;
+    [key: string]: any;
 }
 
 // (async function() {
@@ -58,35 +56,43 @@ interface CloudinaryUploadResult {
 // })();
 
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
     const { userId } = await auth()
 
     if (!userId) {
-        return NextResponse.json({error:"Unauthorized"},{status:401})
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
     }
-    try{
-            const formData = await request.formData();
-            const file = formData.get("file") as File | null
+    try {
+        const formData = await request.formData();
+        const file = formData.get("file") as File | null
 
-            if(!file){
-                return NextResponse.json({error:"File Not Found"},{status:400})
+        if (!file) {
+            return NextResponse.json({ error: "File Not Found" }, { status: 400 })
+        }
+
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
+        // from the above 2 line of code you can upload anything anywhere 
+
+        const result = await new Promise<CloudinaryUploadResult>(
+            (resolve, rejects) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    { folder: "next-cloudinary-upload" },
+                    // inside the folder
+                    (error, result) => {
+                        if (error) rejects(error);
+                        else resolve(result as CloudinaryUploadResult)
+                    }
+                )
+                uploadStream.end(buffer)
             }
+        )
+        return NextResponse.json({ publicId: result.public_id }, { status: 200 })
 
-            const bytes=  await file.arrayBuffer()
-            const buffer= Buffer.from(bytes)
-            // from the above 2 line of code you can upload anything anywhere 
-
-            await new Promise<CloudinaryUploadResult>(
-                (resolve,rejects) =>{
-                    cloudinary.uploader.upload_stream(
-                        {folder:"next-cloudinary-upload"},
-                        // inside the folder
-                    )
-                }
-            )
-
-    }catch{
-
+    } catch (error) {
+        console.log("Upload image Failed",error);
+        return NextResponse.json({error:"Upload image failed"},{status:500})
+        
     }
 
 }
